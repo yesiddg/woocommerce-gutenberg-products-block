f1fb9855f3369ef5addb673e5a118f5284ccce10
Thomas Roberts <thomas.roberts@automattic.com>
2021-08-10 11:10:31 +0100

    Add clear and fill input test util

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -3,4 +3,5 @@ export { findLabelWithText } from './find-label-with-text';
 export { visitBlockPage, visitPostOfType } from './visit-block-page';
 export { getBlockPagePermalink } from './get-block-page-permalink';
 export { getNormalPagePermalink } from './get-normal-page-permalink';
+export { clearAndFillInput } from './clear-and-fill-input';
 export { shopper } from './shopper';
